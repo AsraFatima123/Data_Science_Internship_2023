683b9682396bd02b145e66e93378db6d663bc923
ASRA FATIMA <99815464+AsraFatima123@users.noreply.github.com>
2023-03-02 22:49:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import streamlit as st
-st.header("Data Science Internship 2023")
+st.title(":red[Internship] :blue[App] :green[1] :sunglasses:")
+st.header(":green[basics of web app building]")
+st.subheader(":pink[basic text elements]")
+
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+if btn_click == True:
+    st.subheader("You clicked me :smile:")
+    st.balloons()
+
+CODE = ''' print("Using the print statement")'''
+st.code(CODE, language = 'python')
